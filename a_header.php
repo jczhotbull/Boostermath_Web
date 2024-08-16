f0ca8e0aea18170bb3f1dbbb4b849c8f28179dbb
jczhotbull <desarrolloweb.pvi@gmail.com>
2024-08-16 09:49:04 -0400

    version final

---
+++ a_header.php
@@ -0,0 +1,57 @@
+<?php  ?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+
+<html>
+
+<head>
+
+<!-- Google tag (gtag.js)  <script async src="https://www.googletagmanager.com/gtag/js?id=G-44M6VG9DB5"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-44M6VG9DB5'); </script> -->
+
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TJJMTKXL');</script>
+<!-- End Google Tag Manager -->
+
+
+
+	<meta charset="utf-8">
+
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+
+    <!-- Favicon -->
+
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+
+    <!-- Google Web Fonts -->
+   <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>  -->
+    <link href="font/css2.css" rel="stylesheet"> <!--principal -->
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet"> 
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+
+
+
+
+
+
